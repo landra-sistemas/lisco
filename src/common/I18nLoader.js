020657b339c4d5cd229b42509803591b53b5f58d
iago sardiña <iago.sardina@landrasistemas.es>
2022-06-03 09:26:01 +0200

    prettier y pruebas

--- src/common/I18nLoader.js
+++ src/common/I18nLoader.js
@@ -1,18 +1,13 @@
-import fs from 'fs';
-import path from 'path';
-import util from 'util';
-import Utils from './Utils';
+import fs from "fs";
+import util from "util";
+import Utils from "./Utils";
 
-
-import chokidar from 'chokidar';
+import chokidar from "chokidar";
 
 export default class I18nLoader {
-
-
     constructor() {
         this.watcher = {};
     }
-
 
     /**
      *
@@ -33,55 +28,53 @@ export default class I18nLoader {
 
         // Initialize watcher.
         this.watcher[lang] = chokidar.watch(file, {
-            ignored: /(^|[\/\\])\../, // ignore dotfiles
-            persistent: true
+            ignored: /(^|[/\\])\../, // ignore dotfiles
+            persistent: true,
         });
         //Add change watcher
-        this.watcher[lang]
-            .on('change', path => this.loadFile(path, lang));
+        this.watcher[lang].on("change", (path) => this.loadFile(path, lang));
 
         //Initialize file load
-        await this.loadFile(file, lang)
+        await this.loadFile(file, lang);
     }
 
     /**
      * Carga el archivo de traducciones.
-     * 
-     * @param {*} file 
-     * @param {*} lang 
+     *
+     * @param {*} file
+     * @param {*} lang
      */
     async loadFile(file, lang) {
         const readfile = util.promisify(fs.readFile);
         try {
-            const data = await readfile(file, 'utf8');
+            const data = await readfile(file, "utf8");
             var parsedData = JSON.parse(data);
 
             this.currentDataFlat[lang] = Utils.flattenObject(parsedData);
             this.currentData[lang] = parsedData;
         } catch (ex) {
-            console.error(ex)
-            console.log("Lang file does not exist. Create it on ./i18n/lang_{xx}.json")
+            console.error(ex);
+            console.log("Lang file does not exist. Create it on ./i18n/lang_{xx}.json");
         }
     }
 
     /**
-     * 
-     * @param {*} key 
+     *
+     * @param {*} key
      */
     async translate(key, lang) {
-        if (!lang) lang = process.env.DEFAULT_LANG
+        if (!lang) lang = process.env.DEFAULT_LANG;
 
         if (this.currentDataFlat && this.currentDataFlat[lang] && this.currentDataFlat[lang][key]) {
-            return this.currentData[lang][key]
+            return this.currentData[lang][key];
         }
 
         if (!this.currentDataFlat || !this.currentDataFlat[lang]) {
             await this.load(lang);
             if (this.currentDataFlat && this.currentDataFlat[lang] && this.currentDataFlat[key]) {
-                return this.currentDataFlat[lang][key]
+                return this.currentDataFlat[lang][key];
             }
         }
         return "undefined." + key;
     }
 }
-
